bf439385a61fd3305281147a39533688a3035fe8
Milton Pividori <miltondp@gmail.com>
2022-12-21 16:56:56 -0500

    add GPT3CompletionModel class and tests

---
+++ libs/chatgpt_editor/models.py
@@ -0,0 +1,114 @@
+import os
+import re
+from abc import ABC, abstractmethod
+
+
+class ManuscriptRevisionModel(ABC):
+    """
+    TODO: add docstring
+    """
+
+    def __init__(self):
+        # Get ChatGPT API key and place into github secrets file
+        assert (
+            "OPENAI_API_KEY" in os.environ
+        ), "OPENAI_API_KEY not found in environment variables"
+        self.api_key = os.environ["OPENAI_API_KEY"]
+
+    @abstractmethod
+    def revise_paragraph(self, paragraph_text, section_name):
+        """
+        TODO: add docstring
+
+        Args:
+            paragraph_text (str):
+            section_name (str):
+
+        Returns:
+            Revised paragraph text.
+        """
+        raise NotImplemented
+
+    @abstractmethod
+    def get_prompt(self, paragraph_text, section_name):
+        """
+        TODO: add docstring
+        """
+        raise NotImplemented
+
+
+class GPT3CompletionModel(ManuscriptRevisionModel):
+    """
+    Revises a paragraphs using GPT-3 completion model.
+
+    TODO:
+    - Read OpenAI API parameters: https://beta.openai.com/docs/api-reference/completions/create
+      - There are many that are interesting, like temperature, top_p, presence_penalty, frequency_penalty, etc.
+        "best_of" is interesting, but it can consume a lot of API calls.
+      - Another interesting parameters is "user", it could be "manubot" here.
+    """
+
+    def __init__(
+        self,
+        title: str,
+        keywords: list[str],
+        model_engine: str = "text-davinci-003",
+        temperature: float = 0.9,
+    ):
+        super().__init__()
+
+        self.title = title
+        self.keywords = keywords
+
+        self.model_parameters = {
+            "engine": model_engine,
+            "temperature": temperature,
+        }
+
+        self.several_spaces_pattern = re.compile(r"\s+")
+
+    def get_prompt(self, paragraph_text, section_name):
+        if section_name in ("abstract",):
+            prompt = f"""
+                Revise this {section_name} of an academic paper with title '{self.title}'
+                and keywords '{", ".join(self.keywords)}' to make sure the writing
+                is easy to read, it is in active voice, and the take-home message
+                is clear: {paragraph_text}
+            """
+        elif section_name in ("introduction",):
+            prompt = f"""
+                Revise the following paragraph of the {section_name} section of an academic paper with title '{self.title}'
+                and keywords '{", ".join(self.keywords)}'. Make sure the writing
+                is easy to read, it is in active voice, it has a clear and easy-to-read
+                sentence structure, and it minimizes the use of jargon. The text between brackets
+                are citations to other scientific articles: {paragraph_text}
+            """
+        else:
+            raise ValueError(f"Section name {section_name} not supported")
+
+        return self.several_spaces_pattern.sub(" ", prompt).strip()
+
+    def revise_paragraph(self, paragraph_text, section_name):
+        """
+        TODO: add docstring
+
+        TODO:
+          - Add reduction_fraction, between 0 and 1, which multiplies the paragraph
+            length by this fraction before sending to GPT-3. This is useful to
+            force summarization.
+        """
+        import openai
+
+        paragraph_length = len(paragraph_text)
+        prompt = self.get_prompt(paragraph_text, section_name)
+
+        completions = openai.Completion.create(
+            engine=self.model_parameters["engine"],
+            prompt=prompt,
+            max_tokens=paragraph_length,
+            n=1,
+            stop=None,
+            temperature=self.model_parameters["temperature"],
+        )
+        message = completions.choices[0].text
+        return message
---
+++ tests/test_completion_model.py
@@ -0,0 +1,129 @@
+from chatgpt_editor.models import GPT3CompletionModel
+
+
+def test_model_object_init():
+    model = GPT3CompletionModel(
+        title="Test title",
+        keywords=["test", "keywords"],
+    )
+
+
+def test_get_prompt_for_abstract():
+    paragraph_text = "Text of the abstract"
+    title = (
+        "An efficient not-only-linear correlation coefficient based on machine learning"
+    )
+    keywords = [
+        "correlation coefficient",
+        "nonlinear relationships",
+        "gene expression",
+    ]
+
+    model = GPT3CompletionModel(
+        title=title,
+        keywords=keywords,
+    )
+
+    prompt = model.get_prompt(paragraph_text, "abstract")
+    assert prompt is not None
+    assert isinstance(prompt, str)
+    assert "abstract" in prompt
+    assert f"'{title}'" in prompt
+    assert f"{keywords[0]}" in prompt
+    assert f"{keywords[1]}" in prompt
+    assert f"{keywords[2]}" in prompt
+    assert paragraph_text in prompt
+    assert prompt.startswith("Revise")
+    assert "  " not in prompt
+
+
+def test_get_prompt_for_introduction():
+    paragraph_text = "Text of the initial part."
+    title = (
+        "An efficient not-only-linear correlation coefficient based on machine learning"
+    )
+    keywords = [
+        "correlation coefficient",
+        "nonlinear relationships",
+        "gene expression",
+    ]
+
+    model = GPT3CompletionModel(
+        title=title,
+        keywords=keywords,
+    )
+
+    prompt = model.get_prompt(paragraph_text, "introduction")
+    assert prompt is not None
+    assert isinstance(prompt, str)
+    assert "introduction" in prompt
+    assert f"'{title}'" in prompt
+    assert f"{keywords[0]}" in prompt
+    assert f"{keywords[1]}" in prompt
+    assert f"{keywords[2]}" in prompt
+    assert paragraph_text in prompt
+    assert prompt.startswith("Revise")
+    assert "  " not in prompt
+
+
+def test_revise_abstract():
+    paragraph_text = """
+Correlation coefficients are widely used to identify patterns in data that may be of particular interest.
+In transcriptomics, genes with correlated expression often share functions or are part of disease-relevant biological processes.
+Here we introduce the Clustermatch Correlation Coefficient (CCC), an efficient, easy-to-use and not-only-linear coefficient based on machine learning models.
+CCC reveals biologically meaningful linear and nonlinear patterns missed by standard, linear-only correlation coefficients.
+CCC captures general patterns in data by comparing clustering solutions while being much faster than state-of-the-art coefficients such as the Maximal Information Coefficient.
+When applied to human gene expression data, CCC identifies robust linear relationships while detecting nonlinear patterns associated, for example, with sex differences that are not captured by linear-only coefficients.
+Gene pairs highly ranked by CCC were enriched for interactions in integrated networks built from protein-protein interaction, transcription factor regulation, and chemical and genetic perturbations, suggesting that CCC could detect functional relationships that linear-only methods missed.
+CCC is a highly-efficient, next-generation not-only-linear correlation coefficient that can readily be applied to genome-scale data and other domains across different data types.
+    """.strip()
+
+    model = GPT3CompletionModel(
+        title="An efficient not-only-linear correlation coefficient based on machine learning",
+        keywords=[
+            "correlation coefficient",
+            "nonlinear relationships",
+            "gene expression",
+        ],
+    )
+
+    paragraph_revised = model.revise_paragraph(paragraph_text, "abstract")
+    assert paragraph_revised is not None
+    assert isinstance(paragraph_revised, str)
+    assert paragraph_revised != paragraph_text
+    assert len(paragraph_revised) < len(paragraph_text)
+
+
+def test_revise_introduction_paragraph():
+    paragraph_text = """
+New technologies have vastly improved data collection, generating a deluge of information across different disciplines.
+This large amount of data provides new opportunities to address unanswered scientific questions, provided we have efficient tools capable of identifying multiple types of underlying patterns.
+Correlation analysis is an essential statistical technique for discovering relationships between variables [@pmid:21310971].
+Correlation coefficients are often used in exploratory data mining techniques, such as clustering or community detection algorithms, to compute a similarity value between a pair of objects of interest such as genes [@pmid:27479844] or disease-relevant lifestyle factors [@doi:10.1073/pnas.1217269109].
+Correlation methods are also used in supervised tasks, for example, for feature selection to improve prediction accuracy [@pmid:27006077; @pmid:33729976].
+The Pearson correlation coefficient is ubiquitously deployed across application domains and diverse scientific areas.
+Thus, even minor and significant improvements in these techniques could have enormous consequences in industry and research.
+    """.strip()
+
+    model = GPT3CompletionModel(
+        title="An efficient not-only-linear correlation coefficient based on machine learning",
+        keywords=[
+            "correlation coefficient",
+            "nonlinear relationships",
+            "gene expression",
+        ],
+    )
+
+    paragraph_revised = model.revise_paragraph(paragraph_text, "introduction")
+    assert paragraph_revised is not None
+    assert isinstance(paragraph_revised, str)
+    assert paragraph_revised != paragraph_text
+    assert len(paragraph_revised) < len(paragraph_text)
+
+    # make sure manubot references were kept
+    assert "[@" in paragraph_revised
+    assert "@pmid:21310971" in paragraph_revised
+    assert "@pmid:27479844" in paragraph_revised
+    assert "@pmid:27006077" in paragraph_revised
+    assert "@pmid:33729976" in paragraph_revised
+    assert "@doi:10.1073/pnas.1217269109" in paragraph_revised
