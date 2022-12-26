adb78c66afc9fc55e70817b9d1950c98304ef914
Milton Pividori <miltondp@gmail.com>
2022-12-26 16:29:04 -0500

    setup.py: update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="manubot-chatgpt-editor",
-    version="0.0.3",
+    version="0.0.4",
     author="Milton Pividori",
     author_email="miltondp@gmail.com",
     description="A Manubot plugin to revise a manuscript using GPT-3",
