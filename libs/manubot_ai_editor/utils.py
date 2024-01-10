62a014321df9b5707c70d2ba71d4e8760475a7a0
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2024-01-10 15:03:12 -0700

    Adds ManuscriptPromptConfig, which performs filename->prompt resolution via YAML config files.

--- libs/manubot_ai_editor/utils.py
+++ libs/manubot_ai_editor/utils.py
@@ -24,3 +24,29 @@ def starts_with_similar(string: str, prefix: str, threshold: float = 0.8) -> boo
     return (
         difflib.SequenceMatcher(None, prefix, string[: len(prefix)]).ratio() > threshold
     )
+
+def get_obj_path(target: any, path: tuple, missing=None):
+    """
+    Traverse a nested object using a tuple of keys, returning the last resolved
+    value in the path. If any key is not found, return 'missing' (default None).
+
+    >>> get_obj_path({'a': {'b': {'c': 1}}}, ('a', 'b', 'c'))
+    1
+    >>> get_obj_path({'a': {'b': {'c': 1}}}, ('a', 'b', 'd')) is None
+    True
+    >>> get_obj_path({'a': {'b': {'c': 1}}}, ('a', 'b', 'd'), missing=2)
+    2
+    >>> get_obj_path({'a': [100, {'c': 1}]}, ('a', 1, 'c'))
+    1
+    >>> get_obj_path({'a': [100, {'c': 1}]}, ('a', 1, 'd')) is None
+    True
+    >>> get_obj_path({'a': [100, {'c': 1}]}, ('a', 3)) is None
+    True
+    """
+    try:
+        for key in path:
+            target = target[key]
+    except (KeyError, IndexError, TypeError):
+        return missing
+        
+    return target
